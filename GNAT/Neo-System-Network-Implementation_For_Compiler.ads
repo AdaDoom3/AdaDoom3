8d118045b8b66393e388da403bc3768c9ed2be78
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-02-08 07:12:20 -0800

    Create Neo-System-Network-Implementation_For_Compiler.ads

---
+++ GNAT/Neo-System-Network-Implementation_For_Compiler.ads
@@ -0,0 +1,26 @@
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+with
+  GNAT.Sockets,
+  Neo.Foundation.Data_Types;
+use
+  GNAT.Sockets,
+  Neo.Foundation.Data_Types;
+separate(Neo.System.Network)
+package Implementation_For_Compiler
+  is
+  end Implementation_For_Compiler;
