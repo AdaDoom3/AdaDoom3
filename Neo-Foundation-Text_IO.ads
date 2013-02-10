05897dfcfc1a97782758702853075404611faf5a
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-02-10 18:35:32 -0500

    Update Neo-Foundation-Text_IO.ads

--- Neo-Foundation-Text_IO.ads
+++ Neo-Foundation-Text_IO.ads
@@ -15,95 +15,94 @@
 --
 --
 with
-  Interfaces,
   Ada.Strings,
+  Ada.Text_IO,
   Ada.Wide_Text_IO,
-  Ada.Strings.Wide_Fixed,
+  Ada.Strings.Fixed,
   Neo.Foundation.Data_Types,
   Neo.Foundation.Generic_Protected;
 use
-  Interfaces,
   Ada.Strings,
-  Ada.Wide_Text_IO,
-  Ada.Strings.Wide_Fixed,
+  Ada.Strings.Fixed,
   Neo.Foundation.Data_Types;
 package Neo.Foundation.Text_IO
   is
+  -------------
+  -- Numbers --
+  -------------
+    subtype Integer_Base
+      is Ada.Text_IO.Number_Base;
+    subtype Integer_Positive_Count
+      is Ada.Wide_Text_IO.Positive_Count;
   ---------------
   -- Accessors --
   ---------------
-    type Access_Function_Localize
+    type Access_Subprogram_Localize
       is access function(
         Item : in String_2)
         return String_2;
-    type Access_Procedure_Put
+    type Access_Subprogram_Put
       is access procedure(
         Item : in String_2);
-    type Access_Function_Get_Line
-      is access function
-        return String_2;
+    type Access_Subprogram_Get_Line
+      is access procedure(
+        Item : out String_2;
+        Last : out Integer_4_Natural);
+    type Access_Subprogram_Skip_Line
+      is access procedure(
+        Spacing : in Integer_Positive_Count := 1);
   -----------------
   -- Subprograms --
   -----------------
     procedure Test;
-    procedure Put(
-      Item : in Character_1);
-    procedure Put(
-      Item : in Character_2);
-    procedure Put(
-      Item : in String_1);
-    procedure Put(
-      Item : in String_2);
-    procedure Put_Line(
-      Item : in Character_1);
-    procedure Put_Line(
-      Item : in Character_2);
-    procedure Put_Line(
-      Item : in String_1);
-    procedure Put_Line(
-      Item : in String_2);
     procedure Set(
-      Localize  : in Access_Function_Localize;
-      Put       : in Access_Procedure_Put;
-      Get_Line  : in Access_Function_Get_Line);
-    procedure Set_Localizer(
-      Localizer : in Access_Function_Localizer);
+      New_Line_Size : in Integer_4_Positive;
+      New_Localize  : in Access_Subprogram_Localize;
+      New_Put       : in Access_Subprogram_Put;
+      New_Get_Line  : in Access_Subprogram_Get_Line;
+      New_Skip_Line : in Access_Subprogram_Skip_Line);
+    procedure Set_Line_Size(
+      New_Line_Size : in Integer_4_Positive);
+    procedure Set_Localize(
+      New_Localize : in Access_Subprogram_Localize);
     procedure Set_Put(
-      New_Put : in Access_Put);
+      New_Put : in Access_Subprogram_Put);
     procedure Set_Get_Line(
-      New_Get_Line : in Access_Get_Line);
-    function Get
-      return Character_2;
-    function Get_Line
-      return String_2;
-    procedure Get_Line;
-    function Localize(
-      Item : in String_2)
-      return String_2;
-    function Hexadecimal_Image(
-      Item : in Integer_4_Unsigned)
-      return String_2;
-    function Hexadecimal_Image(
-      Item : in Integer_2_Unsigned)
-      return String_2;
-    function Hexadecimal_Image(
-      Item : in Integer_1_Unsigned)
-      return String_2;
-    function Binary_Image(
-      Item             : in Integer_4_Unsigned;
-      Do_Space_Nibbles : in Boolean := False)
-      return String_2;
-    function Binary_Image(
-      Item             : in Integer_2_Unsigned;
-      Do_Space_Nibbles : in Boolean := False)
-      return String_2;
-    function Binary_Image(
-      Item             : in Integer_1_Unsigned;
-      Do_Space_Nibbles : in Boolean := False)
+      New_Get_Line : in Access_Subprogram_Get_Line);
+    procedure Set_Skip_Line(
+      New_Skip_Line : in Access_Subprogram_Skip_Line);
+    procedure Put(
+      Item        : in Character_2;
+      Do_Localize : in Boolean := False);
+    procedure Put(
+      Item        : in String_2;
+      Do_Localize : in Boolean := False);
+    procedure Put_Line(
+      Item        : in String_2;
+      Do_Localize : in Boolean := False);
+    procedure Get(
+      Item : in out Character_2);
+    procedure Get_Line(
+      Item : in out String_2;
+      Last : in out Integer_4_Natural);
+    function Get_Line_Size
+      return Integer_4_Positive;
+    procedure Skip_Line(
+      Spacing : in Integer_4_Positive := 1);
+    procedure New_Line(
+      Lines : in Integer_4_Positive := 1);
+    function Wide_Image(
+      Item : in Integer_8_Unsigned;
+      Base : in Integer_Base)
       return String_2;
 -------
 private
 -------
+  ---------------
+  -- Constants --
+  ---------------
+    WIDE_IMAGE_BUFFER_SIZE : constant Integer_4_Positive := 256;
+    DEFAULT_LINE_SIZE      : constant Integer_4_Positive := 80;
   -----------------
   -- Subprograms --
   -----------------
@@ -115,17 +114,19 @@ private
   -------------
     type Record_Input_Output
       is record
-        Localize : Access_Function_Localize := Dummy_Localize'Access;
-        Put      : Access_Procedure_Put     := Ada.Wide_Text_IO.Put'Access;
-        Get_Line : Access_Function_Get_Line := Ada.Wide_Text_IO.Get_Line'Access;
+        Line_Size : Integer_4_Positive          := DEFAULT_LINE_SIZE;
+        Localize  : Access_Subprogram_Localize  := null;
+        Put       : Access_Subprogram_Put       := Ada.Wide_Text_IO.Put'Access;
+        Get_Line  : Access_Subprogram_Get_Line  := Ada.Wide_Text_IO.Get_Line'Access;
+        Skip_Line : Access_Subprogram_Skip_Line := Ada.Wide_Text_IO.Skip_Line'Access;
       end record;
   --------------
   -- Packages --
   --------------
-    package Integer_4_Signed_IO
-      is new Ada.Wide_Text_IO.Integer_IO(Integer_4_Signed);
     package Protected_Record_Input_Output
-      is new Foundational.Generic_Protected(Record_Input_Output);
+      is new Neo.Foundation.Generic_Protected(Record_Input_Output);
+    package Integer_8_Unsigned_Text_IO
+      is new Ada.Text_IO.Modular_IO(Integer_8_Unsigned);
   ---------------
   -- Variables --
   ---------------
