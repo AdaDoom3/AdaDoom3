6ecb63d3bde0cdb4626357527e2cbc0aadd4b643
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-02-10 18:37:52 -0500

    Update Neo-Foundation-Package_Testing.adb

--- Neo-Foundation-Package_Testing.adb
+++ Neo-Foundation-Package_Testing.adb
@@ -22,58 +22,66 @@ package body Neo.Foundation.Package_Testing
     procedure Test
       is
       begin
-        Display_Title("PACKAGE TESTING TEST", True);
-        Display_Title("SECOND PART OF THE PACKAGE TESTING TEST FEATURING A VERY LARGE TITLE");
+        Put_Title("PACKAGE TESTING TEST");
+        Put_Title("SECOND PART OF THE PACKAGE_TESTING TEST FEATURING A VERY LARGE TITLE123456123456");
         Hang_Window;
+        --Set_Line_Size(10);
+        --Put_Title("12345678901"); -- Should raise exception
       end Test;
   ---------------
   -- Put_Title --
   ---------------
     procedure Put_Title(
-      Title              : in String_2;
-      Do_Show_Directions : in Boolean := False)
+      Title : in String_2)
       is
       Space_Count : Integer_4_Signed := 0;
+      Count       : Integer_4_Signed := Title'Length * 3 - Title'Length / 3;
       begin
+        if Title'Length > Get_Line_Size then
+          raise Title_Is_Too_Large_To_Fit_On_A_Single_Line;
+        end if;
         New_Line;
-        for I in 1..Get.Line_Size loop
+        for I in 1..Get_Line_Size loop
           Put(TESTING_SEPORATOR);
         end loop;
-        New_Line;
-        New_Line;
+        New_Line(2);
         for I in 1..Title'Length loop
           if Title(I) = ' ' then
             Space_Count := Space_Count + 1;
           end if;
         end loop;
-        for I in 1..(Get.Line_Size / 2 - (Title'Length * 3 - Title'Length / 3 + Space_Count) / 2 - 1) loop
-          Put(" ");
-        end loop;
-        for I in 1..Title'Length loop
-          Put(Title(I) & "  ");
-          if Title(I) = ' ' then
+        if Count + Space_Count >= Get_Line_Size then
+          for I in 1..(Get_Line_Size / 2 - (Title'Length) / 2 - 1) loop
             Put(" ");
-          end if;
-        end loop;
+          end loop;
+          Put(Title);
+        else
+          for I in 1..(Get_Line_Size / 2 - (Count + Space_Count) / 2 - 1) loop
+            Put(" ");
+          end loop;
+          for I in 1..Title'Length loop
+            Put(Title(I) & "  ");
+            if Title(I) = ' ' then
+              Put(" ");
+            end if;
+          end loop;
+        end if;
         New_Line;
-        for I in 1..Get.Line_Size loop
+        for I in 1..Get_Line_Size loop
           Put(TESTING_SEPORATOR);
         end loop;
-        New_Line;
-        New_Line;
-        if Do_Show_Directions then
-          Put_Line(TESTING_INSTRUCTIONS);
-          New_Line;
-        end if;
+        New_Line(2);
       end Put_Title;
     -----------------
     -- Hang_Window --
     -----------------
       procedure Hang_Window
         is
+        Item : String_2(1..1)    := (others => NULL_CHARACTER_2);
+        Last : Integer_4_Natural := 0;
         begin
           New_Line;
           Put(TESTING_INPUT_CURSOR);
-          Get_Line;
+          Get_Line(Item, Last);
         end Hang_Window;
   end Neo.Foundation.Package_Testing;
