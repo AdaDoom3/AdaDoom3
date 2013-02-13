897e6776b6dd2b09d2d934d7961869eef7a7001a
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-02-13 00:15:39 -0500

    Update Windows/Neo-System-Processor-Implementation_For_Operating_System.adb

--- Windows/Neo-System-Processor-Implementation_For_Operating_System.adb
+++ Windows/Neo-System-Processor-Implementation_For_Operating_System.adb
@@ -14,138 +14,141 @@
 --
 --
 --
+with
+  Neo.Windows;
+use
+  Neo.Windows;
 separate(Neo.System.Processor)
 package body Implementation_For_Operating_System
   is
-  --------------------------------------
-  -- Get_Number_Of_Logical_Processors --
-  --------------------------------------
-    function Get_Number_Of_Logical_Processors
-      return Integer_4_Positive
+  -------------------------
+  -- Get_Number_Of_Cores --
+  -------------------------
+    function Get_Number_Of_Cores
+      return Integer_8_Unsigned
       is
-      Function_Get_Logical_Processor_Information :         Access_Get_Logical_Processor_Information := null;
+      --Function_Get_Logical_Processor_Information :         Access_Get_Logical_Processor_Information := null;
       Result                                     :         Integer_4_Natural                        := 0; 
       Size                                       : aliased Integer_4_Unsigned_C                     := 0;
       begin
-        Function_Get_Logical_Processor_Information := 
-          To_Access_Get_Logical_Processor_Information(
-            Get_Procedure_Address(
-              Module         => Get_Module_Handle(To_String_2_C("kernel32")),
-              Procedure_Name => To_String_2_C("GetLogicalProcessorInformation"));
-        if Function_Get_Logical_Processor_Information = null then
-          if USE_64_BIT then
-            -------------
-            Extend_DWORD:
-            -------------
-              declare
-              Process_Affinity : Integer_8_Unsigned_C := 0;
-              System_Affinity  : Integer_8_Unsigned_C := 0;
-              begin
-                if
-                Get_Process_Affinity_Mask(
-                  Process               => Get_Current_Process,
-                  Process_Affinity_Mask => Process_Affinity'Address,
-                  System_Affinity_Mask  => System_Affinity'Address) = FAILED
-                then
-                  return False;
-                end if;
-                if Process_Affinity = 0 and System_Affinity = 0 then
-                  Result.All := 1;
-                  return True;
-                end if;
-                while System_Affinity > 0 loop -- Process_Affinity is not set correctly, but System_Affinity seemed right on test machines
-                  Result          := Result + 1;
-                  System_Affinity := Integer_8_Unsigned_C(Shift_Right(Integer_8_Unsigned(System_Affinity), 1));
-                end loop;
-                Result.All := Integer_4_Positive(Result);
-                return True;
-              end Extend_DWORD;
-          else
-            ------------
-            Leave_DWORD:
-            ------------
-              declare
-              Process_Affinity : Integer_4_Unsigned_C := 0;
-              System_Affinity  : Integer_4_Unsigned_C := 0;
-              begin
-                if
-                Get_Process_Affinity_Mask(
-                  Process               => Get_Current_Process,
-                  Process_Affinity_Mask => Process_Affinity'Address,
-                  System_Affinity_Mask  => System_Affinity'Address) = FAILED
-                then
-                  return False;
-                end if;
-                if Process_Affinity = 0 and System_Affinity = 0 then
-                  Result.All := 1;
-                  return True;
-                end if;
-                while Process_Affinity > 0 loop
-                  Result           := Result + 1;
-                  Process_Affinity := Integer_4_Unsigned_C(Shift_Right(Integer_4_Unsigned(Process_Affinity), 1));
-                end loop;
-                Result.All := Integer_4_Positive(Result);
-                return True;
-              end Leave_DWORD;
-          end if;
-        end if;
-        if 
-        Function_Get_Logical_Processor_Information.All(
-          Buffer        => null,
-          Return_Length => Size'Access) = FAILED and then
-        Get_Last_Error /= ERROR_INSUFFICIENT_BUFFER
-        then
-          raise System_Call_Failure;
-        end if;
-        --------------------------------------
-        Use_Get_Logical_Processor_Information:
-        --------------------------------------
-          declare
-          Information : aliased Array_Record_Logical_Processor_Information(1..Integer_4_Signed(Size)) :=(
-            others => NULL_RECORD_LOGICAL_PROCESSOR_INFORMATION);
-          begin
-            if
-            Function_Get_Logical_Processor_Information.All(
-              Buffer        => Information'Access,
-              Return_Length => Size'Access) = FAILED
-            then
-              raise System_Call_Failure;
-            end if;
-            for I in Information'Range loop
-              if Information.Relationship = localRelationProcessorCore then
-                Result.All := Result.All + Count_Set_Bits(ptr->ProcessorMask);
-              end if;
-            end loop;
-          end Use_Get_Logical_Processor_Information;
-        return ;
-      end Get_Number_Of_Logical_Processors;
+        raise System_Call_Failure; return 1;
+        -- Function_Get_Logical_Processor_Information := 
+        --   To_Access_Get_Logical_Processor_Information(
+        --     Get_Procedure_Address(
+        --       Module         => Get_Module_Handle(To_String_2_C("kernel32")),
+        --       Procedure_Name => To_String_2_C("GetLogicalProcessorInformation")));
+        -- if Function_Get_Logical_Processor_Information = null then
+        --   if USE_64_BIT then
+        --     -------------
+        --     Extend_DWORD:
+        --     -------------
+        --       declare
+        --       Process_Affinity : Integer_8_Unsigned_C := 0;
+        --       System_Affinity  : Integer_8_Unsigned_C := 0;
+        --       begin
+        --         if
+        --         Get_Process_Affinity_Mask(
+        --           Process               => Get_Current_Process,
+        --           Process_Affinity_Mask => Process_Affinity'Address,
+        --           System_Affinity_Mask  => System_Affinity'Address) = FAILED
+        --         then
+        --           raise System_Call_Failure;
+        --         end if;
+        --         if Process_Affinity = 0 and System_Affinity = 0 then
+        --           Result.All := 1;
+        --           return True;
+        --         end if;
+        --         while System_Affinity > 0 loop -- Process_Affinity is not set correctly, but System_Affinity seemed right on test machines
+        --           Result          := Result + 1;
+        --           System_Affinity := Integer_8_Unsigned_C(Shift_Right(Integer_8_Unsigned(System_Affinity), 1));
+        --         end loop;
+        --         Result.All := Integer_4_Positive(Result);
+        --         return True;
+        --       end Extend_DWORD;
+        --   else
+        --     ------------
+        --     Leave_DWORD:
+        --     ------------
+        --       declare
+        --       Process_Affinity : Integer_4_Unsigned_C := 0;
+        --       System_Affinity  : Integer_4_Unsigned_C := 0;
+        --       begin
+        --         if
+        --         Get_Process_Affinity_Mask(
+        --           Process               => Get_Current_Process,
+        --           Process_Affinity_Mask => Process_Affinity'Address,
+        --           System_Affinity_Mask  => System_Affinity'Address) = FAILED
+        --         then
+        --           raise System_Call_Failure;
+        --         end if;
+        --         if Process_Affinity = 0 and System_Affinity = 0 then
+        --           Result.All := 1;
+        --           return Result;
+        --         end if;
+        --         while Process_Affinity > 0 loop
+        --           Result           := Result + 1;
+        --           Process_Affinity := Integer_4_Unsigned_C(Shift_Right(Integer_4_Unsigned(Process_Affinity), 1));
+        --         end loop;
+        --         return Result;
+        --       end Leave_DWORD;
+        --   end if;
+        -- end if;
+        -- if 
+        -- Function_Get_Logical_Processor_Information.All(
+        --   Buffer        => null,
+        --   Return_Length => Size'Access) = FAILED and then
+        -- Get_Last_Error /= ERROR_INSUFFICIENT_BUFFER
+        -- then
+        --   raise System_Call_Failure;
+        -- end if;
+        -- --------------------------------------
+        -- Use_Get_Logical_Processor_Information:
+        -- --------------------------------------
+        --   declare
+        --   Information : aliased Array_Record_Logical_Processor_Information(1..Integer_4_Signed(Size)) :=(
+        --     others => NULL_RECORD_LOGICAL_PROCESSOR_INFORMATION);
+        --   begin
+        --     if
+        --     Function_Get_Logical_Processor_Information.All(
+        --       Buffer        => Information'Access,
+        --       Return_Length => Size'Access) = FAILED
+        --     then
+        --       raise System_Call_Failure;
+        --     end if;
+        --     for I in Information'Range loop
+        --       null;
+        --       --if Information.Relationship = localRelationProcessorCore then
+        --       --  Result.All := Result.All + Count_Set_Bits(ptr->ProcessorMask);
+        --       --end if;
+        --     end loop;
+        --   end Use_Get_Logical_Processor_Information;
+        -- return Result;
+      end Get_Number_Of_Cores;
   ---------------------
   -- Get_Clock_Ticks --
   ---------------------
-    function Get_Clock_Ticks(
-      Result : in Access_Integer_8_Unsigned)
-      return Boolean
+    function Get_Clock_Ticks
+      return Integer_8_Unsigned
       is
-      Ticks : Integer_8_Unsigned_C := 0;
+      Ticks : aliased Integer_8_Unsigned_C := 0;
       begin
         if Query_Performance_Counter(Ticks'Address) = FAILED then
-          return False;
+          raise System_Call_Failure;
         end if;
-        Result.All := Integer_8_Unsigned(Ticks);
-        return True;
+        return Integer_8_Unsigned(Ticks);
       end Get_Clock_Ticks;
   ----------------------------
   -- Get_Speed_In_Megahertz --
   ----------------------------
-    function Get_Speed_In_Megahertz(
-      Result : in Access_Integer_8_Unsigned)
-      return Boolean
+    function Get_Speed_In_Megahertz
+      return Integer_8_Unsigned
       is
+      Result : aliased Integer_8_Unsigned_C := 0;
       begin
-        if Query_Performance_Frequency(Result) = FAILED then
-          -----------------
-          Use_Other_Method:
-          -----------------
+        if Query_Performance_Frequency(Result'Address) = FAILED then
+          ---------------------
+          Look_In_The_Registry:
+          ---------------------
             declare
             Key    : Address              := NULL_ADDRESS;
             Length : Integer_4_Unsigned_C := 1;
@@ -181,15 +184,14 @@ package body Implementation_For_Operating_System
                   Data_Size  => Length'Address) /= NO_ERROR
                 then
                   Dummy := Registry_Close_Key(Key);
-                  return False;
+                  raise System_Call_Failure;
                 end if;
-                Dummy      := Registry_Close_Key(Key);
-                Result.All := Integer_8_Unsigned(Speed * 1_000_000);
-                return True;
+                Dummy := Registry_Close_Key(Key);
+                return Integer_8_Unsigned(Speed * 1_000_000);
               end if;
-              return False;
-            end Use_Other_Method;
+              raise System_Call_Failure;
+            end Look_In_The_Registry;
         end if;
-        return True;
+        return Integer_8_Unsigned(Result);
       end Get_Speed_In_Megahertz;
   end Implementation_For_Operating_System;
