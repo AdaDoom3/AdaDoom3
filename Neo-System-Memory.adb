ddde22c08dd30c1786deef1be5431093a5bcb01a
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-02-13 00:17:30 -0500

    Update Neo-System-Memory.adb

--- Neo-System-Memory.adb
+++ Neo-System-Memory.adb
@@ -16,45 +16,41 @@
 --
 package body Neo.System.Memory
   is
+  --------------------
+  -- Implementation --
+  --------------------
+    package body Implementation
+      is separate;
   ----------
   -- Test --
   ----------
     procedure Test
       is
-      Memory : Record_Memory := NULL_RECORD_MEMORY;
+      Memory : Record_Memory;-- := <>;
       begin
-        Display_Title("SYSTEM TEST");
-        Put;
+        Put_Title("MEMORY TEST");
+        Memory := Get_Data;
+        Put_Line("Load"                       & Float_4_Percent'Wide_Image(Memory.Load));
+        Put_Line("Physical_Total"             & Integer_8_Natural'Wide_Image(Memory.Physical_Total));
+        Put_Line("Physical_Available"         & Integer_8_Natural'Wide_Image(Memory.Physical_Available));
+        Put_Line("Page_File_Total"            & Integer_8_Natural'Wide_Image(Memory.Page_File_Total));
+        Put_Line("Page_File_Available"        & Integer_8_Natural'Wide_Image(Memory.Page_File_Available));
+        Put_Line("Virtual_Total"              & Integer_8_Natural'Wide_Image(Memory.Virtual_Total));
+        Put_Line("Virtual_Available"          & Integer_8_Natural'Wide_Image(Memory.Virtual_Available));
+        Put_Line("Virtual_Available_Extended" & Integer_8_Natural'Wide_Image(Memory.Virtual_Available_Extended));
         Hang_Window;
       end Test;
-  ---------
-  -- Put --
-  ---------
-    procedure Put
-      is
-      Memory : Record_Memory := NULL_RECORD_MEMORY;
-      begin
-        Memory := Get_Memory_Status;
-        Put_Line(TAB & "Load"                       & Float_Percent'Wide_Image(Memory.Load));
-        Put_Line(TAB & "Physical_Total"             & Integer_8_Natural'Wide_Image(Memory.Physical_Total));
-        Put_Line(TAB & "Physical_Available"         & Integer_8_Natural'Wide_Image(Memory.Physical_Available));
-        Put_Line(TAB & "Page_File_Total"            & Integer_8_Natural'Wide_Image(Memory.Page_File_Total));
-        Put_Line(TAB & "Page_File_Available"        & Integer_8_Natural'Wide_Image(Memory.Page_File_Available));
-        Put_Line(TAB & "Virtual_Total"              & Integer_8_Natural'Wide_Image(Memory.Virtual_Total));
-        Put_Line(TAB & "Virtual_Available"          & Integer_8_Natural'Wide_Image(Memory.Virtual_Available));
-        Put_Line(TAB & "Virtual_Available_Extended" & Integer_8_Natural'Wide_Image(Memory.Virtual_Available_Extended));
-      end Put;
-  ---------
-  -- Set --
-  ---------
-    procedure Set(
-      Minimum_Number_Of_Bytes : in Integer_4_Natural;
-      Maximum_Number_Of_Bytes : in Integer_4_Natural)
-      renames Set(Minimum_Number_Of_Bytes, Maximum_Number_Of_Bytes);
-  ---------
-  -- Get --
-  ---------
-    function Get
+  ---------------------
+  -- Set_Byte_Limits --
+  ---------------------
+    procedure Set_Byte_Limits(
+      Minimum : in Integer_4_Unsigned;
+      Maximum : in Integer_4_Unsigned)
+      renames Implementation.Set_Byte_Limits;
+  --------------
+  -- Get_Data --
+  --------------
+    function Get_Data
       return Record_Memory
       renames Implementation.Get;
   ----------
@@ -64,7 +60,7 @@ package body Neo.System.Memory
       Location        : in Address;
       Number_Of_Bytes : in Integer_4_Unsigned)
       return Boolean
-      renames Implementation.Lock(Location, Number_Of_Bytes);
+      renames Implementation.Lock;
   ------------
   -- Unlock --
   ------------
@@ -72,18 +68,24 @@ package body Neo.System.Memory
       Location        : in Address;
       Number_Of_Bytes : in Integer_4_Unsigned)
       return Boolean
-      renames Implementation.Unlock(Location, Number_Of_Bytes);
+      renames Implementation.Unlock;
+  ----------
+  -- Free --
+  ----------
+    procedure Free(
+      Item : in Address)
+      renames Implementation.Free;
   --------------
   -- Allocate --
   --------------
     function Allocate(
-      Number_Of_Bits    : in Integer_4_Positive;
+      Number_Of_Bits    : in Integer_4_Unsigned;
       Memory_Identifier : in Integer_Memory_Identifier := UNASSIGNED_IDENTIFIER)
       return Address
       is
       begin
         return
-          Clear(
+          Implementation.Clear(
             Location      => Allocate_Dirty(Number_Of_Bits, Memory_Identifier),
             Initial_Value => CLEARED_MEMORY_VALUE,
             Size          => Number_Of_Bits);
@@ -92,23 +94,17 @@ package body Neo.System.Memory
   -- Allocate_Dirty --
   --------------------
     function Allocate_Dirty(
-      Number_Of_Bits    : in Integer_4_Positive;
+      Number_Of_Bits    : in Integer_4_Unsigned;
       Memory_Identifier : in Integer_Memory_Identifier := UNASSIGNED_IDENTIFIER)
       return Address
       is
       begin
         return
-          Allocate(
+          Implementation.Allocate(
             Size =>
-              Integer_4_Positive(
-                (Integer_4_Unsigned(Number_Of_Bits) + (MEMORY_ALIGNMENT - 1))
+              Integer_4_Unsigned(
+                (Number_Of_Bits + (MEMORY_ALIGNMENT - 1))
                 and not (MEMORY_ALIGNMENT - 1)),
             Alignment => MEMORY_ALIGNMENT);
       end Allocate_Dirty;
-  ----------
-  -- Free --
-  ----------
-    procedure Free(
-      Item : in Address)
-      renames Free(Item);
   end Neo.System.Memory;
