32840f8349997e1fc7afbd60825c68459c06e9a8
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-03-12 03:50:56 -0300

    Update Neo-System.adb

--- Neo-System.adb
+++ Neo-System.adb
@@ -14,75 +14,86 @@
 --
 --
 --
-package body Neo.System
-  is
+PACKAGE BODY Neo.System
+  IS
   --------------------
   -- Implementation --
   --------------------
-    package body Implementation
-      is separate;
+    PACKAGE BODY Implementation
+      IS SEPARATE;
   ----------
   -- Test --
   ----------
-    procedure Test
-      is
-      begin
+    PROCEDURE Test
+      IS
+      BEGIN
         Put_Title("SYSTEM TEST");
         Put_Line("Language " & Enumerated_Language'Wide_Image(Get_Language));
-        Put_Line("Version "  & Enumerated_Operating_System'Wide_Image(Get_Operating_System));
+        Put_Line("Version "  & Enumerated_System'Wide_Image(Get_Version));
         Put_Line("Username """ & Get_Username & """");
         Open_Webpage("http://www.google.com");
         Execute_Application(False, "C:\Windows\System32\taskmgr.exe");
-        Put_Line("Newer than Windows XP is " & Boolean'Wide_Image(Is_Newer_Than(Windows_2_5_1_System)));
+        --Put_Line("Newer than Windows XP IS " & Boolean'Wide_Image(Is_Newer_Than(Windows_2_5_1_System)));
         Hang_Window;
-      end Test;
+      END Test;
   -------------------
   -- Is_Newer_Than --
   -------------------
-    function Is_Newer_Than(
-      Linux     : in Enumerated_Linux_System;
-      Macintosh : in Enumerated_Macintosh_System;
-      Windows   : in Enumerated_Windows_System)
-      return Boolean
-      is
+    FUNCTION Is_Newer_Than(
+      Linux     : IN Enumerated_Linux_System;
+      Macintosh : IN Enumerated_Macintosh_System;
+      Windows   : IN Enumerated_Windows_System)
+      RETURN Boolean
+      IS
       Current_System : Enumerated_System := Get_Version;
-      begin
-        -- if Operating_System in Enumerated_Linux_System'Range then
-        -- elsif Operating_System in Enumerated_Windows_System'Range then
-        -- elsif Operating_System in Enumerated_Macintosh_System'Range then
-
-        -- end if;
-        return False;
-      end Is_Newer_Than;
+      BEGIN
+        -- if Operating_System IN Enumerated_Linux_System'Range then
+        -- elsif Operating_System IN Enumerated_Windows_System'Range then
+        -- elsif Operating_System IN Enumerated_Macintosh_System'Range then
+        -- END if;
+        RETURN True;
+      END Is_Newer_Than;
+  --------------------------------------
+  -- Is_Running_In_64_Bit_Environment --
+  --------------------------------------
+    FUNCTION Is_Running_In_64_Bit_Environment
+      RETURN Boolean
+      IS
+      BEGIN
+        IF USE_64_BIT THEN
+          RETURN True;
+        END IF;
+        RETURN Implementation.Is_Running_In_64_Bit_Environment;
+      END Is_Running_In_64_Bit_Environment;
   ------------------
   -- Get_Language --
   ------------------
-    function Get_Language
-      return Enumerated_Language
-      renames Implementation.Get_Language;
+    FUNCTION Get_Language
+      RETURN Enumerated_Language
+      RENAMES Implementation.Get_Language;
   -----------------
   -- Get_Version --
   -----------------
-    function Get_Version
-      return Enumerated_System
-      renames Implementation.Get_Version;
+    FUNCTION Get_Version
+      RETURN Enumerated_System
+      RENAMES Implementation.Get_Version;
   ------------------
   -- Get_Username --
   ------------------
-    function Get_Username
-      return String_2
-      renames Implementation.Get_Username;
+    FUNCTION Get_Username
+      RETURN String_2
+      RENAMES Implementation.Get_Username;
   ------------------
   -- Open_Webpage --
   ------------------
-    procedure Open_Webpage(
-      Path : in String_2)
-      renames Implementation.Open_Webpage;
+    PROCEDURE Open_Webpage(
+      Path : IN String_2)
+      RENAMES Implementation.Open_Webpage;
   -------------------------
   -- Execute_Application --
   -------------------------
-    procedure Execute_Application(
-      Do_Quit         : in Boolean;
-      Executable_Path : in String_2)
-      renames Implementation.Execute_Application;
-  end Neo.System;
+    PROCEDURE Execute_Application(
+      Do_Quit         : IN Boolean;
+      Executable_Path : IN String_2)
+      RENAMES Implementation.Execute_Application;
+  END Neo.System;
