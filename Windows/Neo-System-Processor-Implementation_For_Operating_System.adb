a05b205110bce6ebf43beab128eab98c8284aa4c
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-03-14 01:36:21 -0300

    Update Neo-System-Processor-Implementation_For_Operating_System.adb

--- Windows/Neo-System-Processor-Implementation_For_Operating_System.adb
+++ Windows/Neo-System-Processor-Implementation_For_Operating_System.adb
@@ -14,184 +14,196 @@
 --
 --
 --
-with
+WITH
+  System,
+  Interfaces.C,
   Neo.Windows;
-use
+USE
+  System,
+  Interfaces.C,
   Neo.Windows;
-separate(Neo.System.Processor)
-package body Implementation_For_Operating_System
-  is
+SEPARATE(Neo.System.Processor)
+PACKAGE BODY Implementation_For_Operating_System
+  IS
   -------------------------
   -- Get_Number_Of_Cores --
   -------------------------
-    function Get_Number_Of_Cores
-      return Integer_8_Unsigned
-      is
+    FUNCTION Get_Number_Of_Cores
+      RETURN Integer_8_Unsigned
+      IS
       --Function_Get_Logical_Processor_Information :         Access_Get_Logical_Processor_Information := null;
       Result                                     :         Integer_4_Natural                        := 0; 
-      Size                                       : aliased Integer_4_Unsigned_C                     := 0;
-      begin
-        raise System_Call_Failure; return 1;
+      Size                                       : ALIASED Integer_4_Unsigned_C                     := 0;
+      BEGIN
+        RAISE System_Call_Failure; RETURN 1;
         -- Function_Get_Logical_Processor_Information := 
         --   To_Access_Get_Logical_Processor_Information(
         --     Get_Procedure_Address(
         --       Module         => Get_Module_Handle(To_String_2_C("kernel32")),
         --       Procedure_Name => To_String_2_C("GetLogicalProcessorInformation")));
-        -- if Function_Get_Logical_Processor_Information = null then
-        --   if USE_64_BIT then
+        -- IF Function_Get_Logical_Processor_Information = null THEN
+        --   IF USE_64_BIT THEN
         --     -------------
         --     Extend_DWORD:
         --     -------------
-        --       declare
+        --       DECLARE
         --       Process_Affinity : Integer_8_Unsigned_C := 0;
         --       System_Affinity  : Integer_8_Unsigned_C := 0;
-        --       begin
-        --         if
+        --       BEGIN
+        --         IF
         --         Get_Process_Affinity_Mask(
         --           Process               => Get_Current_Process,
         --           Process_Affinity_Mask => Process_Affinity'Address,
         --           System_Affinity_Mask  => System_Affinity'Address) = FAILED
-        --         then
-        --           raise System_Call_Failure;
-        --         end if;
-        --         if Process_Affinity = 0 and System_Affinity = 0 then
+        --         THEN
+        --           RAISE System_Call_Failure;
+        --         END IF;
+        --         IF Process_Affinity = 0 and System_Affinity = 0 THEN
         --           Result.All := 1;
-        --           return True;
-        --         end if;
-        --         while System_Affinity > 0 loop -- Process_Affinity is not set correctly, but System_Affinity seemed right on test machines
+        --           RETURN True;
+        --         END IF;
+        --         while System_Affinity > 0 loop -- Process_Affinity IS not set correctly, but System_Affinity seemed right on test machines
         --           Result          := Result + 1;
         --           System_Affinity := Integer_8_Unsigned_C(Shift_Right(Integer_8_Unsigned(System_Affinity), 1));
-        --         end loop;
+        --         END loop;
         --         Result.All := Integer_4_Positive(Result);
-        --         return True;
-        --       end Extend_DWORD;
+        --         RETURN True;
+        --       END Extend_DWORD;
         --   else
         --     ------------
         --     Leave_DWORD:
         --     ------------
-        --       declare
+        --       DECLARE
         --       Process_Affinity : Integer_4_Unsigned_C := 0;
         --       System_Affinity  : Integer_4_Unsigned_C := 0;
-        --       begin
-        --         if
+        --       BEGIN
+        --         IF
         --         Get_Process_Affinity_Mask(
         --           Process               => Get_Current_Process,
         --           Process_Affinity_Mask => Process_Affinity'Address,
         --           System_Affinity_Mask  => System_Affinity'Address) = FAILED
-        --         then
-        --           raise System_Call_Failure;
-        --         end if;
-        --         if Process_Affinity = 0 and System_Affinity = 0 then
+        --         THEN
+        --           RAISE System_Call_Failure;
+        --         END IF;
+        --         IF Process_Affinity = 0 and System_Affinity = 0 THEN
         --           Result.All := 1;
-        --           return Result;
-        --         end if;
+        --           RETURN Result;
+        --         END IF;
         --         while Process_Affinity > 0 loop
         --           Result           := Result + 1;
         --           Process_Affinity := Integer_4_Unsigned_C(Shift_Right(Integer_4_Unsigned(Process_Affinity), 1));
-        --         end loop;
-        --         return Result;
-        --       end Leave_DWORD;
-        --   end if;
-        -- end if;
-        -- if 
+        --         END loop;
+        --         RETURN Result;
+        --       END Leave_DWORD;
+        --   END IF;
+        -- END IF;
+        -- IF 
         -- Function_Get_Logical_Processor_Information.All(
         --   Buffer        => null,
-        --   Return_Length => Size'Access) = FAILED and then
+        --   Return_Length => Size'Access) = FAILED and THEN
         -- Get_Last_Error /= ERROR_INSUFFICIENT_BUFFER
-        -- then
-        --   raise System_Call_Failure;
-        -- end if;
+        -- THEN
+        --   RAISE System_Call_Failure;
+        -- END IF;
         -- --------------------------------------
         -- Use_Get_Logical_Processor_Information:
         -- --------------------------------------
-        --   declare
-        --   Information : aliased Array_Record_Logical_Processor_Information(1..Integer_4_Signed(Size)) :=(
+        --   DECLARE
+        --   Information : ALIASED Array_Record_Logical_Processor_Information(1..Integer_4_Signed(Size)) :=(
         --     others => NULL_RECORD_LOGICAL_PROCESSOR_INFORMATION);
-        --   begin
-        --     if
+        --   BEGIN
+        --     IF
         --     Function_Get_Logical_Processor_Information.All(
         --       Buffer        => Information'Access,
         --       Return_Length => Size'Access) = FAILED
-        --     then
-        --       raise System_Call_Failure;
-        --     end if;
+        --     THEN
+        --       RAISE System_Call_Failure;
+        --     END IF;
         --     for I in Information'Range loop
         --       null;
-        --       --if Information.Relationship = localRelationProcessorCore then
+        --       --IF Information.Relationship = localRelationProcessorCore THEN
         --       --  Result.All := Result.All + Count_Set_Bits(ptr->ProcessorMask);
-        --       --end if;
-        --     end loop;
-        --   end Use_Get_Logical_Processor_Information;
-        -- return Result;
-      end Get_Number_Of_Cores;
+        --       --END IF;
+        --     END loop;
+        --   END Use_Get_Logical_Processor_Information;
+        -- RETURN Result;
+      END Get_Number_Of_Cores;
   ---------------------
   -- Get_Clock_Ticks --
   ---------------------
-    function Get_Clock_Ticks
-      return Integer_8_Unsigned
-      is
-      Ticks : aliased Integer_8_Unsigned_C := 0;
-      begin
-        if Query_Performance_Counter(Ticks'Address) = FAILED then
-          raise System_Call_Failure;
-        end if;
-        return Integer_8_Unsigned(Ticks);
-      end Get_Clock_Ticks;
+    FUNCTION Get_Clock_Ticks
+      RETURN Integer_8_Unsigned
+      IS
+      Ticks : ALIASED Integer_8_Unsigned_C := 0;
+      BEGIN
+        IF Query_Performance_Counter(Ticks'Address) = FAILED THEN
+          RAISE System_Call_Failure;
+        END IF;
+        RETURN Integer_8_Unsigned(Ticks);
+      END Get_Clock_Ticks;
   ----------------------------
   -- Get_Speed_In_Megahertz --
   ----------------------------
-    function Get_Speed_In_Megahertz
-      return Integer_8_Unsigned
-      is
-      Result : aliased Integer_8_Unsigned_C := 0;
-      begin
-        if Query_Performance_Frequency(Result'Address) = FAILED then
-          ---------------------
-          Look_In_The_Registry:
-          ---------------------
-            declare
-            Key    : Address              := NULL_ADDRESS;
-            Length : Integer_4_Unsigned_C := 1;
-            Speed  : Integer_4_Unsigned_C := 0;
-            Dummy  : Integer_4_Signed_C   := 0;
-            begin
-              if Registry_Open_Key(
-                Key     => HKEY_LOCAL_MACHINE,
-                Sub_key => To_String_2_C("HARDWARE\DESCRIPTION\System\CentralProcessor\0"),
-                Options => 0,
-                Desired => KEY_READ,
-                Result  => Key'Address) = NO_ERROR then
-                if Registry_Query_Value(
-                  Key        => Key,
-                  Value_Name => To_String_2_C("~MHz"),
-                  Reserved   => NULL_ADDRESS,
-                  Kind       => NULL_ADDRESS,
-                  Data       => Speed'Address,
-                  Data_Size  => Length'Address) /= NO_ERROR and then
-                Registry_Query_Value(
-                  Key        => Key,
-                  Value_Name => To_String_2_C("~Mhz"),
-                  Reserved   => NULL_ADDRESS,
-                  Kind       => NULL_ADDRESS,
-                  Data       => Speed'Address,
-                  Data_Size  => Length'Address) /= NO_ERROR and then
-                Registry_Query_Value(
-                  Key        => Key,
-                  Value_Name => To_String_2_C("~mhz"),
-                  Reserved   => NULL_ADDRESS,
-                  Kind       => NULL_ADDRESS,
-                  Data       => Speed'Address,
-                  Data_Size  => Length'Address) /= NO_ERROR
-                then
-                  Dummy := Registry_Close_Key(Key);
-                  raise System_Call_Failure;
-                end if;
-                Dummy := Registry_Close_Key(Key);
-                return Integer_8_Unsigned(Speed * 1_000_000);
-              end if;
-              raise System_Call_Failure;
-            end Look_In_The_Registry;
-        end if;
-        return Integer_8_Unsigned(Result);
-      end Get_Speed_In_Megahertz;
-  end Implementation_For_Operating_System;
+    FUNCTION Get_Speed_In_Megahertz
+      RETURN Integer_8_Unsigned
+      IS
+      Result : ALIASED Integer_8_Unsigned_C := 0;
+      BEGIN
+        IF Query_Performance_Frequency(Result'Address) /= FAILED THEN
+          RETURN Integer_8_Unsigned(Result);
+        END IF;
+        ---------------------
+        Look_In_The_Registry:
+        ---------------------
+          DECLARE
+          Key      : Address              := NULL_ADDRESS;
+          Length   : Integer_4_Unsigned_C := 1;
+          Speed    : Integer_4_Unsigned_C := 0;
+          Do_Raise : Boolean              := False; 
+          BEGIN
+            IF
+            Registry_Open_Key(
+              Key     => HKEY_LOCAL_MACHINE,
+              Sub_key => To_String_2_C("HARDWARE\DESCRIPTION\System\CentralProcessor\0"),
+              Options => 0,
+              Desired => KEY_READ,
+              Result  => Key'Address) /= NO_ERROR
+            THEN
+              RAISE System_Call_Failure;
+            END IF;
+            IF
+            Registry_Query_Value(
+              Key        => Key,
+              Value_Name => To_String_2_C("~MHz"),
+              Reserved   => NULL_ADDRESS,
+              Kind       => NULL_ADDRESS,
+              Data       => Speed'Address,
+              Data_Size  => Length'Address) /= NO_ERROR and THEN
+            Registry_Query_Value(
+              Key        => Key,
+              Value_Name => To_String_2_C("~Mhz"),
+              Reserved   => NULL_ADDRESS,
+              Kind       => NULL_ADDRESS,
+              Data       => Speed'Address,
+              Data_Size  => Length'Address) /= NO_ERROR and THEN
+            Registry_Query_Value(
+              Key        => Key,
+              Value_Name => To_String_2_C("~mhz"),
+              Reserved   => NULL_ADDRESS,
+              Kind       => NULL_ADDRESS,
+              Data       => Speed'Address,
+              Data_Size  => Length'Address) /= NO_ERROR
+            THEN
+              Do_Raise := True;
+            END IF;
+            IF Registry_Close_Key(Key) /= NO_ERROR THEN
+              null;
+            END IF;
+            IF Do_Raise THEN
+              RAISE System_Call_Failure;
+            END IF;
+            RETURN Integer_8_Unsigned(Speed * 1_000_000);
+          END Look_In_The_Registry;
+      END Get_Speed_In_Megahertz;
+  END Implementation_For_Operating_System;
+
