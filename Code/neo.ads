d98387b91f9a8cf3ff34e4f41855df0181cb9fb9
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-04-22 18:12:39 -0300

    Update neo.ads

--- Code/neo.ads
+++ Code/neo.ads
@@ -25,9 +25,4 @@ package Neo
   ---------------
     NAME    : constant String := "AdaDoom3";
     VERSION : constant Float  := -1.0;
-
-  Private
-    -- Added a renames so that the Package "System" sould be accessable
-    -- further down the package heiarchy (particularly Neo.System).
-    Package Ada_Sys Renames System;
   end Neo;
