89b9d3935f964f5eb5d9096cae8a3f58f4f022e1
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-04-22 19:13:34 -0300

    Update neo-system-implementation_for_architecture.adb

--- Code/x64/neo-system-implementation_for_architecture.adb
+++ Code/x64/neo-system-implementation_for_architecture.adb
@@ -1,1 +1,29 @@
-...
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+separate(Neo.System)
+package Implementation_For_Architecture
+  is
+  -----------------------
+  -- Is_Sixty_Four_Bit --
+  -----------------------
+    function Is_Sixty_Four_Bit
+      return Boolean
+      is
+      begin
+        return True;
+      end Is_Sixty_Four_Bit;
+  end Implementation_For_Architecture;
