cbc87049c95d094572f7a316a734037147bea941
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-04-22 19:14:36 -0300

    Update and rename placeholder.txt to neo-system-implementation_for_architecture.adb

---
+++ Code/x32/neo-system-implementation_for_architecture.adb
@@ -0,0 +1,29 @@
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+separate(Neo.System)
+package Implementation_For_Architecture
+  is
+  -----------------------
+  -- Is_Sixty_Four_Bit --
+  -----------------------
+    function Is_Sixty_Four_Bit
+      return Boolean
+      is
+      begin
+        return False;
+      end Is_Sixty_Four_Bit;
+  end Implementation_For_Architecture;
