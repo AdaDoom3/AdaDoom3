1ed4d8bfaaeedd9fcf398ec8848ef9037c3fcaa2
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-04-23 23:04:57 -0300

    Update neo-system-text.ads

--- Code/neo-system-text.ads
+++ Code/neo-system-text.ads
@@ -15,33 +15,19 @@
 --
 --
 with
-  Ada.Finalization,
   Neo.Foundation.Text_IO,
   Neo.Foundation.Data_Types,
   Neo.Foundation.Package_Testing;
 use
-  Ada.Finalization,
   Neo.Foundation.Text_IO,
   Neo.Foundation.Data_Types,
   Neo.Foundation.Package_Testing;
 package Neo.System.Text
   is
-  -----------
-  -- Types --
-  -----------
-    type Record_String_UTF8
-      is private;
   ----------------
   -- Suprograms --
   ----------------
     procedure Test;
-    function Get(
-      Item  : in Record_String_UTF8;
-      Index : in Integer_4_Positive)
-      return Character_2;
-    function Get_Length(
-      Item : in Record_String_UTF8)
-      return Integer_4_Positive;
     function Get_Clipboard
       return String_2;
     procedure Set_Clipboard(
