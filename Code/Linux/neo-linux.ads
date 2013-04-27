4ae97e009ed93c0a09f0ba90e2d94e948899f0fe
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-04-27 13:11:24 -0300

    Update neo-linux.ads

--- Code/Linux/neo-linux.ads
+++ Code/Linux/neo-linux.ads
@@ -14,6 +14,29 @@
 --
 --
 --
+with
+  Interfaces.C,
+  Neo.Foundation.Data_Types;
+use
+  Interfaces.C,
+  Neo.Foundation.Data_Types;
 package Neo.Linux
   is
+  ---------------
+  -- Constants --
+  ---------------
+    SC_NPROCESSORS_ONLN : constant := 84;
+  -----------------
+  -- Subprograms --
+  -----------------
+    function Sysconf(
+      Name : Integer_4_Signed_C)
+      return Long;
+-------
+private
+-------
+  ----------------
+  -- Directives --
+  ----------------
+    pragma Import(C, Sysconf, "sysconf");
   end Neo.Linux;
