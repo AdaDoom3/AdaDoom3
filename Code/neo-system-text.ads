4fd3e5bebaaab62cb85a46f04e2ccbcfc0d913f3
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-01 21:24:33 -0300

    Update neo-system-text.ads

--- Code/neo-system-text.ads
+++ Code/neo-system-text.ads
@@ -28,6 +28,8 @@ package Neo.System.Text
   -- Suprograms --
   ----------------
     procedure Test;
+    function Get_Language
+      return Enumerated_Language;
     function Get_Clipboard
       return String_2;
     procedure Set_Clipboard(
@@ -40,9 +42,11 @@ private
   --------------------
     package Implementation
       is
+        function Get_Language
+          return Enumerated_Language;
         procedure Set_Clipboard(
           Text : in String_2);
         function Get_Clipboard
           return String_2;
       end Implementation;
-  end Neo.System.Text;
+end Neo.System.Text;
