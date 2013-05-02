97113cb1a4c0a87b8a44e5083da0a69b41aafd71
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-01 21:14:12 -0300

    Update neo-system.ads

--- Code/neo-system.ads
+++ Code/neo-system.ads
@@ -26,14 +26,25 @@ use
   Neo.Foundation.Package_Testing;
 package Neo.System
   is
-  ----------------
-  -- Exceptions --
-  ----------------
-    System_Call_Failure : Exception;
-    Unsupported_Feature : Exception;
+  ---------------
+  -- Constants --
+  ---------------
+    DEFAULT_USERNAME : constant String_2 := "Unnamed";
   ------------------
   -- Enumerations --
   ------------------
+    type Enumerated_Icon
+      is(
+      No_Icon,
+      Warning_Icon,
+      Information_Icon,
+      Error_Icon);
+    type Enumerated_Buttons
+      is(
+      Yes_No_Buttons,
+      Okay_Button,
+      Okay_Cancel_Buttons,
+      Retry_Cancel_Buttons);
     type Enumerated_Language
       is(
       Arabic_Language,
@@ -67,19 +78,15 @@ package Neo.System
     type Enumerated_System
       is(
       Unknown_System,
-      Linux_System, -- PowerPC?, x86-64?
-      Linux_2_System,
+      Linux_System,
+      Linux_2_System, -- x86-64
       Linux_2_1_System,
       Linux_2_2_System,
       Linux_2_3_System,
       Linux_2_4_System,
       Linux_2_5_System,
       Linux_2_6_System,
-      Linux_2_7_System, -- AVX
-      Linux_2_8_System,
-      Linux_2_9_System,
-      Linux_3_System,
-      Linux_3_0_System,
+      Linux_3_System, -- AVX
       Linux_3_1_System,
       Linux_3_2_System,
       Linux_3_3_System,
@@ -89,8 +96,8 @@ package Neo.System
       Linux_3_7_System,
       Linux_3_8_System,
       Linux_3_9_System,
-      Windows_System, -- x86-64
-      Windows_1_4_A_System,
+      Windows_System,
+      Windows_1_4_A_System, -- x86-64
       Windows_1_4_B_System,
       Windows_1_4_10_A_System,
       Windows_1_4_10_B_System,
@@ -101,30 +108,20 @@ package Neo.System
       Windows_2_6_System, -- Aero™
       Windows_2_6_1_System, -- AVX
       Windows_2_6_2_System,
-      Macintosh_System, -- PowerPC
-      Macintosh_8_5_System,
-      Macintosh_8_5_1_System,
+      Macintosh_System,
+      Macintosh_8_5_System, -- PowerPC
       Macintosh_8_6_System,
-      Macintosh_9_System,
-      Macintosh_9_0_1_System,
-      Macintosh_9_0_2_System,
-      Macintosh_9_0_3_System,
-      Macintosh_9_0_4_System,
+      Macintosh_9_0_System,
       Macintosh_9_1_System,
       Macintosh_9_2_System,
-      Macintosh_9_2_1_System,
-      Macintosh_9_2_2_System,
       Macintosh_10_System,
       Macintosh_10_1_System,
       Macintosh_10_2_System,
       Macintosh_10_3_System,
       Macintosh_10_4_System,
-      Macintosh_10_4_7_System, -- x86-64
-      Macintosh_10_5_System,
-      Macintosh_10_5_8_System, -- PPC RIP
+      Macintosh_10_5_System, -- x86-64
       Macintosh_10_6_System,
-      Macintosh_10_6_8_System, -- AVX
-      Macintosh_10_7_System,
+      Macintosh_10_7_System, -- AVX
       Macintosh_10_8_System);
     subtype Enumerated_Linux_System
       is Enumerated_System
@@ -135,26 +132,24 @@ package Neo.System
     subtype Enumerated_Macintosh_System
       is Enumerated_System
       range Macintosh_System..Macintosh_10_8_System;
-  -----------------
-  -- Subprograms --
-  -----------------
+  ----------------
+  -- Suprograms --
+  ----------------
     procedure Test;
-    function Get_Language
-      return Enumerated_Language;
     function Get_Version
       return Enumerated_System;
     function Get_Username
       return String_2;
-    procedure Open_Webpage(
-      Path : in String_2);
-    procedure Execute_Application(
-      Do_Quit         : in Boolean;
-      Executable_Path : in String_2);
     function Is_Newer_Than(
       Linux     : in Enumerated_Linux_System;
       Macintosh : in Enumerated_Macintosh_System;
       Windows   : in Enumerated_Windows_System)
       return Boolean;
+    procedure Open_Webpage(
+      Path : in String_2);
+    procedure Execute_Application(
+      Do_Quit         : in Boolean;
+      Executable_Path : in String_2);
     function Is_Running_In_Emulated_32_Bit_Environment
       return Boolean;
 -------
@@ -167,8 +162,6 @@ private
       is
         function Is_Running_In_Emulated_32_Bit_Environment
           return Boolean;
-        function Get_Language
-          return Enumerated_Language;
         function Get_Version
           return Enumerated_System;
         function Get_Username
