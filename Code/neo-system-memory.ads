ea9f30d0506fad4cdec8a25e20dd3edcd857d9cc
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-03 18:59:29 -0300

    Update neo-system-memory.ads

--- Code/neo-system-memory.ads
+++ Code/neo-system-memory.ads
@@ -27,17 +27,8 @@ use
   Neo.Foundation.Text_IO,
   Neo.Foundation.Data_Types,
   Neo.Foundation.Package_Testing;
-package Neo.System.Memory -- Memory allocation, all in one place
+package Neo.System.Memory
   is
-  ----------------
-  -- Exceptions --
-  ----------------
-    -- Lock, unlock stuff here
-  -------------
-  -- Numbers --
-  -------------
-    type Integer_Memory_Identifier
-      is new Integer_Address;
   -------------
   -- Records --
   -------------
@@ -53,34 +44,15 @@ package Neo.System.Memory -- Memory allocation, all in one place
         Virtual_Available          : Integer_8_Natural;
         Virtual_Available_Extended : Integer_8_Natural;
       end record;
-  ---------------
-  -- Constants --
-  ---------------
-    CLEARED_MEMORY_VALUE      : constant Boolean                   := False;
-    UNASSIGNED_IDENTIFIER     : constant Integer_Memory_Identifier := 0;
-    MEMORY_ALIGNMENT_IN_BYTES : constant Integer_4_Unsigned        := 2;
   --------------
   -- Packages --
   --------------
     generic
       type Type_To_Manage
         is private;
-      Manager_Identifier : Integer_Memory_Identifier := UNASSIGNED_IDENTIFIER;
-    package Manager -- wip
+    package Manager
       is
-        type Array_Type_To_Manage
-          is array(Positive range <>)
-          of Type_To_Manage;
-        function Allocate
-          return Type_To_Manage;
-        function Allocate(
-          Number_To_Allocate : in Integer_4_Positive)
-          return Array_Type_To_Manage;
-        function Allocate_Dirty
-          return Type_To_Manage;
-        function Allocate_Dirty(
-          Number_To_Allocate : in Integer_4_Positive)
-          return Array_Type_To_Manage;
+        -- Lock, unlock exceptions here
         procedure Lock(
           Item : in out Type_To_Manage);
         procedure Unlock(
@@ -91,8 +63,9 @@ package Neo.System.Memory -- Memory allocation, all in one place
   -----------------
     procedure Test;
     procedure Set_Byte_Limits(
-      Minimum : in Integer_4_Unsigned;
-      Maximum : in Integer_4_Unsigned);
+      Minimum : in Integer_Address;
+      Maximum : in Integer_Address)
+      with Pre => Minimum < Maximum;
     function Get_State
       return Record_State;
 -------
@@ -106,22 +79,14 @@ private
         function Get_State
           return Record_State;
         procedure Set_Byte_Limits(
-          Minimum : in Integer_4_Unsigned;
-          Maximum : in Integer_4_Unsigned);
+          Minimum : in Integer_Address;
+          Maximum : in Integer_Address)
+          with Pre => Minimum < Maximum;
         procedure Lock(
           Location        : in Address;
-          Number_Of_Bytes : in Integer_4_Unsigned);
+          Number_Of_Bytes : in Integer_Address);
         procedure Unlock(
           Location        : in Address;
-          Number_Of_Bytes : in Integer_4_Unsigned);
-        function Clear(
-          Location      : in Address;
-          Size          : in Integer_4_Unsigned;
-          Initial_Value : in Boolean := CLEARED_MEMORY_VALUE)
-          return Address;
-        function Allocate(
-          Size      : in Integer_4_Unsigned;
-          Alignment : in Integer_4_Unsigned)
-          return Address;
+          Number_Of_Bytes : in Integer_Address);
       end Implementation;
   end Neo.System.Memory;
