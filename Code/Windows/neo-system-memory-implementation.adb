9632c1e47da9a5c14bafc41883b8281bd5588cc6
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-03 20:23:48 -0300

    Update neo-system-memory-implementation.adb

--- Code/Windows/neo-system-memory-implementation.adb
+++ Code/Windows/neo-system-memory-implementation.adb
@@ -38,15 +38,16 @@ package body Implementation
         end if;
         return(
           -- For some reason, Total_Physical is sometimes off by a meg or two, so round up to the nearest 16 megs
-          Physical_Total             => Integer_8_Natural((Status.Total_Physical / (1024 * 1024) + 8) and 16#FFFF_FFFF_FFFF_FFF0#),
-          Physical_Available         => Integer_8_Natural(Status.Available_Physical),
-          Page_File_Total            => Integer_8_Natural(Status.Total_Page_File),
-          Page_File_Available        => Integer_8_Natural(Status.Available_Page_File),
-          Virtual_Total              => Integer_8_Natural(Status.Total_Virtual),
-          Virtual_Available          => Integer_8_Natural(Status.Available_Virtual),
-          Virtual_Available_Extended => Integer_8_Natural(Status.Available_Extended_Virtual),
-          Load                       => Float_4_Percent(Status.Memory_Load),
-          Freespace_In_Gigabytes     => );
+          Physical_Total             => Integer_8_Unsigned((Status.Total_Physical / (1024 * 1024) + 8) and 16#FFFF_FFFF_FFFF_FFF0#),
+          Physical_Available         => Integer_8_Unsigned(Status.Available_Physical),
+          Page_File_Total            => Integer_8_Unsigned(Status.Total_Page_File),
+          Page_File_Available        => Integer_8_Unsigned(Status.Available_Page_File),
+          Virtual_Total              => Integer_8_Unsigned(Status.Total_Virtual),
+          Virtual_Available          => Integer_8_Unsigned(Status.Available_Virtual),
+          Virtual_Available_Extended => Integer_8_Unsigned(Status.Available_Extended_Virtual),
+          Disk_Total                 => Integer_8_Unsigned(),
+          Disk_Available             => Integer_8_Unsigned(),
+          Load                       => Float_4_Percent(Status.Memory_Load));
       end Get_Status;
   ---------------------
   -- Set_Byte_Limits --
@@ -70,11 +71,11 @@ package body Implementation
   ----------
     procedure Lock(
       Location        : in Address;
-      Number_Of_Bytes : in Integer_4_Unsigned)
+      Number_Of_Bytes : in Integer_Address)
       is
       begin
         if Virtual_Lock(Location, Integer_Size_C(Number_Of_Bytes)) = FAILED then
-          raise ???;
+          raise System_Call_Failure;
         end if;
       end Lock;
   ------------
@@ -82,11 +83,11 @@ package body Implementation
   ------------
     procedure Unlock(
       Location        : in Address;
-      Number_Of_Bytes : in Integer_4_Unsigned)
+      Number_Of_Bytes : in Integer_Address)
       is
       begin
         if Virtual_Unlock(Location, Integer_Size_C(Number_Of_Bytes)) = FAILED then
-          raise ???;
+          raise System_Call_Failure;
         end if;
       end Unlock;
   end Implementation;
