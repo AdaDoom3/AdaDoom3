16e5b2950a3e47be168b1cdae8c767b0995b4748
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-03 05:55:30 -0300

    Update neo-macintosh.ads

--- Code/Macintosh/neo-macintosh.ads
+++ Code/Macintosh/neo-macintosh.ads
@@ -14,6 +14,31 @@
 --
 --
 --
+with
+  System,
+  Interfaces,
+  Interfaces.C,
+  Neo.Foundation.Data_Types;
+use
+  System,
+  Interfaces,
+  Interfaces.C,
+  Neo.Foundation.Data_Types;
 package Neo.Macintosh
   is
+  ---------------
+  -- Constants --
+  ---------------
+  -----------
+  -- Types --
+  -----------
+  -----------------
+  -- Subprograms --
+  -----------------
+-------
+private
+-------
+  ----------------
+  -- Directives --
+  ----------------
   end Neo.Macintosh;
