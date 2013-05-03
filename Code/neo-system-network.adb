51902c4a38f20f17841fc6fd3caa9c1632cc7a79
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-03 12:28:45 -0300

    Update neo-system-network.adb

--- Code/neo-system-network.adb
+++ Code/neo-system-network.adb
@@ -14,186 +14,186 @@
 --
 --
 --
-PACKAGE BODY Neo.System.Network
-  IS
+package body Neo.System.Network
+  is
   --------------------
-  -- IMPLEMENTATION --
+  -- Implementation --
   --------------------
-    PACKAGE BODY Implementation
-      IS SEPARATE;
+    package body Implementation
+      is separate;
   ----------------
   -- Initialize --
   ----------------
-    PROCEDURE Initialize
-      RENAMES Implementation.Initialize;
+    procedure Initialize
+      renames Implementation.Initialize;
   --------------
   -- Finalize --
   --------------
-    PROCEDURE Finalize
-      RENAMES Implementation.Finalize;
+    procedure Finalize
+      renames Implementation.Finalize;
   -----------------
   -- Set_Address --
   -----------------
-    PROCEDURE Set_Address(
-      Connection      : IN OUT Record_Connection;
-      Network_Address : IN     Record_Network_Address)
-      RENAMES Implementation.Set_Address;
+    procedure Set_Address(
+      Connection      : in out Record_Connection;
+      Network_Address : in     Record_Network_Address)
+      renames Implementation.Set_Address;
   -------------
   -- Recieve --
   -------------
-    FUNCTION Recieve(
-      Connection : IN  Record_Connection;
-      From       : OUT Record_Network_Address)
-      RETURN Array_Integer_1_Unsigned
-      RENAMES Implementation.Recieve;
-    FUNCTION Recieve(
-      Connection : IN  Record_Connection;
-      From       : OUT Record_Network_Address;
-      Timeout    : IN  Duration)
-      RETURN Array_Integer_1_Unsigned
-      RENAMES Implementation.Recieve;
+    function Recieve(
+      Connection : in  Record_Connection;
+      From       : out Record_Network_Address)
+      return Array_Integer_1_Unsigned
+      renames Implementation.Recieve;
+    function Recieve(
+      Connection : in  Record_Connection;
+      From       : out Record_Network_Address;
+      Timeout    : in  Duration)
+      return Array_Integer_1_Unsigned
+      renames Implementation.Recieve;
   ----------
   -- Send --
   ----------
-    PROCEDURE Send(
-      Connection : IN Record_Connection;
-      To         : IN Record_Network_Address;
-      Data       : IN Array_Integer_1_Unsigned)
-      RENAMES Implementation.Send;
+    procedure Send(
+      Connection : in Record_Connection;
+      To         : in Record_Network_Address;
+      Data       : in Array_Integer_1_Unsigned)
+      renames Implementation.Send;
   ------------------
   -- Get_Local_IP --
   ------------------
-    FUNCTION Get_Local_IP
-      RETURN String_2
-      RENAMES Implementation.Get_Local_IP;
+    function Get_Local_IP
+      return String_2
+      renames Implementation.Get_Local_IP;
   -------------------------
   -- Get_Network_Address --
   -------------------------
-    FUNCTION Get_Network_Address(
-      Connection : IN Record_Connection)
-      RETURN Record_Network_Address
-      IS
-      BEGIN
-        RETURN Connection.Network_Address;
-      END Get_Network_Address;
+    function Get_Network_Address(
+      Connection : in Record_Connection)
+      return Record_Network_Address
+      is
+      begin
+        return Connection.Network_Address;
+      end Get_Network_Address;
   --------------------------------
   -- Get_Number_Of_Read_Packets --
   --------------------------------
-    FUNCTION Get_Number_Of_Read_Packets(
-      Connection : IN Record_Connection)
-      RETURN Integer_4_Natural
-      IS
-      BEGIN
-        RETURN Connection.Number_Of_Read_Packets;
-      END Get_Number_Of_Read_Packets;
+    function Get_Number_Of_Read_Packets(
+      Connection : in Record_Connection)
+      return Integer_4_Natural
+      is
+      begin
+        return Connection.Number_Of_Read_Packets;
+      end Get_Number_Of_Read_Packets;
   ------------------------------
   -- Get_Number_Of_Read_Bytes --
   ------------------------------
-    FUNCTION Get_Number_Of_Read_Bytes(
-      Connection : IN Record_Connection)
-      RETURN Integer_4_Natural
-      IS
-      BEGIN
-        RETURN Connection.Number_Of_Read_Bytes;
-      END Get_Number_Of_Read_Bytes;
+    function Get_Number_Of_Read_Bytes(
+      Connection : in Record_Connection)
+      return Integer_4_Natural
+      is
+      begin
+        return Connection.Number_Of_Read_Bytes;
+      end Get_Number_Of_Read_Bytes;
   -----------------------------------
   -- Get_Number_Of_Written_Packets --
   -----------------------------------
-    FUNCTION Get_Number_Of_Written_Packets(
-      Connection : IN Record_Connection)
-      RETURN Integer_4_Natural
-      IS
-      BEGIN
-        RETURN Connection.Number_Of_Written_Packets;
-      END Get_Number_Of_Written_Packets;
+    function Get_Number_Of_Written_Packets(
+      Connection : in Record_Connection)
+      return Integer_4_Natural
+      is
+      begin
+        return Connection.Number_Of_Written_Packets;
+      end Get_Number_Of_Written_Packets;
   ---------------------------------
   -- Get_Number_Of_Written_Bytes --
   ---------------------------------
-    FUNCTION Get_Number_Of_Written_Bytes(
-      Connection : IN Record_Connection)
-      RETURN Integer_4_Natural
-      IS
-      BEGIN
-        RETURN Connection.Number_Of_Written_Bytes;
-      END Get_Number_Of_Written_Bytes;
+    function Get_Number_Of_Written_Bytes(
+      Connection : in Record_Connection)
+      return Integer_4_Natural
+      is
+      begin
+        return Connection.Number_Of_Written_Bytes;
+      end Get_Number_Of_Written_Bytes;
   -------------
   -- Silence --
   -------------
-    PROCEDURE Silence(
-      Connection : IN OUT Record_Connection)
-      IS
-      BEGIN
-        IF Connection.Is_Silenced THEN
-          RAISE SILENCED_WITHOUT_BEING_VOCAL;
-        END IF;
+    procedure Silence(
+      Connection : in out Record_Connection)
+      is
+      begin
+        if Connection.Is_Silenced then
+          raise Silenced_Without_Being_Vocal;
+        end if;
         Connection.Is_Silenced := True;
-      END Silence;
+      end Silence;
   --------------
   -- Vocalize --
   --------------
-    PROCEDURE Vocalize(
-      Connection : IN OUT Record_Connection)
-      IS
-      BEGIN
-        IF NOT Connection.Is_Silenced THEN
-          RAISE VOCALIZED_WITHOUT_BEING_SILENT;
-        END IF;
+    procedure Vocalize(
+      Connection : in out Record_Connection)
+      is
+      begin
+        if not Connection.Is_Silenced then
+          raise Vocalized_Without_Being_Silent
+        end if;
         Connection.Is_Silenced := False;
-      END Silence;
+      end Silence;
   ------------------------
   -- To_Network_Address --
   ------------------------
-    FUNCTION To_Network_Address(
-      Item : IN String_2)
-      RETURN Record_Network_Address
-      IS
+    function To_Network_Address(
+      Item : in String_2)
+      return Record_Network_Address
+      is
       Result   : Record_Network_Address := (OTHERS => <>);
       Previous : Integer_4_Signed       := Item'First;
       J        : Integer_4_Signed       := Result.IP'First;
-      BEGIN
-        FOR I IN Item'Range LOOP
-          IF Item(I) = ':' THEN
+      begin
+        for I in Item'Range loop
+          if Item(I) = ':' then
 
-          ELSIF Item(I) = '.' THEN
+          elsif Item(I) = '.' then
             Result.IP(J) := ;
             J := J + 1;
-          END IF;
-        END LOOP;
-      EXCEPTION
+          end if;
+        end LOOP;
+      exception
         WHEN 
-      END To_Network_Address;
+      end To_Network_Address;
   -----------------
   -- To_String_2 --
   -----------------
-    FUNCTION To_String_2(
-      Network_Address : IN Record_Network_Address)
-      RETURN String_2
-      IS
-      BEGIN
-        RETURN
+    function To_String_2(
+      Network_Address : in Record_Network_Address)
+      return String_2
+      is
+      begin
+        return
           Trim(Integer_1_Unsigned'Wide_Image(Network_Address.IP(1)), Both) & "." &
           Trim(Integer_1_Unsigned'Wide_Image(Network_Address.IP(2)), Both) & "." & 
           Trim(Integer_1_Unsigned'Wide_Image(Network_Address.IP(3)), Both) & "." & 
           Trim(Integer_1_Unsigned'Wide_Image(Network_Address.IP(4)), Both) & ":" &
           Trim(Integer_2_Unsigned'Wide_Image(Network_Address.Port),  Both);
-      END To_String_2;
+      end To_String_2;
   ---------
   -- "=" --
   ---------
-    FUNCTION "="(
-      Left  : IN Record_Network_Address;
-      Right : IN String_2)
-      RETURN Boolean
-      IS
-      BEGIN
-        RETURN Left = To_String_2(Right);
-      END "=";
-    FUNCTION "="(
-      Left  : IN String_2;
-      Right : IN Record_Network_Address)
-      RETURN Boolean
-      IS
-      BEGIN
-        RETURN Right = Left;
-      END "=";
-  END Neo.System.Network;
+    function "="(
+      Left  : in Record_Network_Address;
+      Right : in String_2)
+      return Boolean
+      is
+      begin
+        return Left = To_String_2(Right);
+      end "=";
+    function "="(
+      Left  : in String_2;
+      Right : in Record_Network_Address)
+      return Boolean
+      is
+      begin
+        return Right = Left;
+      end "=";
+  end Neo.System.Network;
