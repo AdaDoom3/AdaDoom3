076ca2df0e350c47b227e661d0fc96c3677bef68
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-03 20:35:20 -0300

    Update neo-system-network.ads

--- Code/neo-system-network.ads
+++ Code/neo-system-network.ads
@@ -24,9 +24,9 @@ package Neo.System.Network
   ----------------
   -- Exceptions --
   ----------------
-    Silenced_Without_Being_Vocal      : Exception;
-    Vocalized_without_Begin_Silent    : Exception;
-    Invalid_Network_Address_Submitted : Exception;
+    Silenced_Without_Being_Vocal   : Exception;
+    Vocalized_without_Begin_Silent : Exception;
+    Invalid_Network_Address_Passed : Exception;
   -----------
   -- Types --
   -----------
@@ -47,13 +47,13 @@ package Neo.System.Network
       return Integer_4_Natural;
     function Get_Number_Of_Read_Bytes(
       Connection : in Record_Connection)
-      RETURN Integer_4_Natural;
+      return Integer_4_Natural;
     function Get_Number_Of_Written_Packets(
       Connection : in Record_Connection)
-      RETURN Integer_4_Natural;
+      return Integer_4_Natural;
     function Get_Number_Of_Written_Bytes(
       Connection : in Record_Connection)
-      RETURN Integer_4_Natural;
+      return Integer_4_Natural;
     procedure Set_Address(
       Connection      : in out Record_Connection;
       Network_Address : in     String_2);
@@ -61,28 +61,10 @@ package Neo.System.Network
       Connection : in out Record_Connection);
     procedure Vocalize(
       Connection : in out Record_Connection);
-    function To_Network_Address(
-      Item : in String_2)
-      return Record_Network_Address;
-    function To_String_2(
-      Network_Address : in Record_Network_Address)
-      return String_2;
-    function "="(
-      Left  : in Record_Network_Address;
-      Right : in String_2)
-      return Boolean;
-    function "="(
-      Left  : in String_2;
-      Right : in Record_Network_Address)
-      return Boolean;
-    function Recieve(
-      Connection : in  Record_Connection;
-      From       : out Record_Network_Address)
-      return Array_Integer_1_Unsigned;
     function Recieve(
       Connection : in  Record_Connection;
       From       : out Record_Network_Address;
-      Timeout    : in  Duration)
+      Timeout    : in  Duration := 0.0)
       return Array_Integer_1_Unsigned;
     procedure Send(
       Connection : in Record_Connection;
@@ -97,18 +79,18 @@ private
     type Record_Connection
       is Ada.Controlled.Limited_Controlled
       with record
-        Network_Address           : Record_Network_Address := (others => <>);
-        Socket                    : Integer_4_Natural      := 0;
-        Is_Silenced               : Boolean                := False;
-        Number_Of_Read_Packets    : Integer_4_Natural      := 0;
-        Number_Of_Read_Bytes      : Integer_4_Natural      := 0;
-        Number_Of_Written_Packets : Integer_4_Natural      := 0;
-        Number_Of_Written_Bytes   : Integer_4_Natural      := 0;
+        Network_Address           : String_1(1..64)   := (others => NULL_CHARACTER_1);
+        Socket                    : Integer_4_Natural := 0;
+        Is_Silenced               : Boolean           := False;
+        Number_Of_Read_Packets    : Integer_4_Natural := 0;
+        Number_Of_Read_Bytes      : Integer_4_Natural := 0;
+        Number_Of_Written_Packets : Integer_4_Natural := 0;
+        Number_Of_Written_Bytes   : Integer_4_Natural := 0;
       end record;
   -----------------
   -- Subprograms --
   -----------------
-    PROCEDURE Finalize(
+    procedure Finalize(
       Item : in out Record_Connection);
   --------------------
   -- Implementation --
