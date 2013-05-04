07641a4c3f1a77d0f52f83be7b5bbdef5e062851
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-04 08:51:24 -0300

    Update neo-system-memory.adb

--- Code/neo-system-memory.adb
+++ Code/neo-system-memory.adb
@@ -52,18 +52,20 @@ package body Neo.System.Memory
   ----------
     procedure Test
       is
-      Status : Record_Status := (others => <>);
+      Status : Record_State := (others => <>);
       begin
         Put_Title("MEMORY TEST");
-        Memory := Get_Status;
+        Memory := Get_State;
         Put_Line("Load: "                       & Float_4_Percent'Wide_Image(Memory.Load));
-        Put_Line("Physical total: "             & Integer_Address'Wide_Image(Memory.Physical_Total));
-        Put_Line("Physical available: "         & Integer_Address'Wide_Image(Memory.Physical_Available));
-        Put_Line("Page file total: "            & Integer_Address'Wide_Image(Memory.Page_File_Total));
-        Put_Line("Page file available: "        & Integer_Address'Wide_Image(Memory.Page_File_Available));
-        Put_Line("Virtual total: "              & Integer_Address'Wide_Image(Memory.Virtual_Total));
-        Put_Line("Virtual available: "          & Integer_Address'Wide_Image(Memory.Virtual_Available));
-        Put_Line("Virtual available extended: " & Integer_Address'Wide_Image(Memory.Virtual_Available_Extended));
+        Put_Line("Disk total: "                 & Integer_Address'Wide_Image(Memory.Number_Of_Disk_Bytes_Total));
+        Put_Line("Disk available: "             & Integer_Address'Wide_Image(Memory.Number_Of_Disk_Bytes_Available));
+        Put_Line("Physical total: "             & Integer_Address'Wide_Image(Memory.Number_Of_Physical_Bytes_Total));
+        Put_Line("Physical available: "         & Integer_Address'Wide_Image(Memory.Number_Of_Physical_Bytes_Available));
+        Put_Line("Page file total: "            & Integer_Address'Wide_Image(Memory.Number_Of_Page_File_Bytes_Total));
+        Put_Line("Page file available: "        & Integer_Address'Wide_Image(Memory.Number_Of_Page_File_Bytes_Available));
+        Put_Line("Virtual total: "              & Integer_Address'Wide_Image(Memory.Number_Of_Virtual_Bytes_Total));
+        Put_Line("Virtual available: "          & Integer_Address'Wide_Image(Memory.Number_Of_Virtual_Bytes_Available));
+        Put_Line("Virtual available extended: " & Integer_Address'Wide_Image(Memory.Number_Of_Virtual_Bytes_Available_Extended));
         Hang_Window;
       end Test;
   ---------------------
@@ -80,17 +82,17 @@ package body Neo.System.Memory
         when System_Call_Failure =>
           null;
       end Set_Byte_Limits;
-  ----------------
-  -- Get_Status --
-  ----------------
-    function Get_Status
+  ---------------
+  -- Get_State --
+  ---------------
+    function Get_State
       return Record_Memory
       is
       begin
-        return Implementation.Get_Data;
+        return Implementation.Get_State;
       exception
         when System_Call_Failure =>
           return (others => <>);
-      end Get_Status;
+      end Get_State;
   end Neo.System.Memory;
 
