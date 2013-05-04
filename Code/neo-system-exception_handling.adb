7f5ba07f3ae6da32f45998ec9c5e5e2cd4525d1a
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-04 06:51:09 -0300

    Update neo-system-exception_handling.adb

--- Code/neo-system-exception_handling.adb
+++ Code/neo-system-exception_handling.adb
@@ -16,12 +16,33 @@
 --
 package body Neo.System.Exception_Handling
   is
+  ----------------------------
+  -- Protected_Alert_Status --
+  ----------------------------
+    protected body Protected_Alert_Status
+      is
+        function Is_Alerting
+          return Boolean
+          is
+          begin
+            return Status;
+          end Is_Alerting;
+        procedure Set_Is_Alerting(
+          New_Status : in Boolean)
+          is
+          begin
+            Status := New_Status;
+          end Is_Alerting;
+      private
+        Status : Boolean := False;
+      end Protected_Alert_Status;
   ----------
   -- Test --
   ----------
     procedure Test
       is
       begin
+        Put_Title("EXCEPTION HANDLING TEST");
       end Test;
   -----------------
   -- Start_Alert --
@@ -30,7 +51,13 @@ package body Neo.System.Exception_Handling
       is
       begin
         if Alert_Status.Is_Alerting then
+          raise Alert_Started_Before_Stop;
         end if;
+        Implementation.Start_Alert;
+        Alert_Status.Set_Is_Alerting(True);
+      exception
+        when System_Call_Failure =>
+          null;
       end Start_Alert;
   ----------------
   -- Stop_Alert --
@@ -38,6 +65,14 @@ package body Neo.System.Exception_Handling
     procedure Stop_Alert
       is
       begin
+        if not Alert_Status.Is_Alerting then
+          raise Alert_Stopped_Without_Start;
+        end if;
+        Implementation.Stop_Alert;
+        Alert_Status.Set_Is_Alerting(False);
+      exception
+        when System_Call_Failure =>
+          null;
       end Stop_Alert;
   --------------------------
   -- Create_Error_Console --
@@ -47,7 +82,7 @@ package body Neo.System.Exception_Handling
       Buttons : in Array_Console_Buttons)
       is
       begin
-        Implementation.Create_Error_Console;
+        Implementation.Create_Error_Console(Text, Buttons);
       exception
         when System_Call_Failure =>
           null;
@@ -64,30 +99,20 @@ package body Neo.System.Exception_Handling
       return Boolean
       is
       begin
+        Implementation.Is_Okay(
+          Title        => Title,
+          Message      => Message,
+          Buttons      => Buttons,
+          Icon         => Icon,
+          Parent_Title => Parent_Title);
+      exception
+        when System_Call_Failure =>
+          return False;
       end Is_Okay;
   -----------------
   -- Is_Alerting --
   -----------------
     function Is_Alerting
       return Boolean
-      is
-      begin
-      end Is_Alerting;
-  ---------------
-  -- Protected --
-  ---------------
-    protected type Protected_Alert_Status
-      is
-        function Is_Alerting
-          return Boolean;
-        procedure Set_Is_Alerting(
-          Status : in Boolean);
-      private
-        Status : Boolean := False;
-      end Protected_Alert_Status;
-  ---------------
-  -- Variables --
-  ---------------
-    Alert_Status : Protected_Alert_Status;
-
+      renames Alert_Status.Is_Alerting;
   end Neo.System.Exception_Handling;
