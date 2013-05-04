68d435c6e131bc3b8ba78940728daa3849f0afb7
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-04 11:17:05 -0300

    Create neo-system-exception_handling-implementation.adb

---
+++ Code/Windows/neo-system-exception_handling-implementation.adb
@@ -0,0 +1,24 @@
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+with
+  Neo.Windows;
+use
+  Neo.Windows;
+separate(Neo.System.Exception_Handling)
+package body Implementation
+  is
+  end Implementation;
