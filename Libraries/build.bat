8eebce18aa35515be239249e94c18d1b338e2afe
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-05 10:23:18 -0300

    Update build.bat

--- Libraries/build.bat
+++ Libraries/build.bat
@@ -1,1 +1,3 @@
 .... TODO
+* overwrite all dlls with new versions if the compiler exists to create for the desired bit size library executable
+* the dll should all be in the source directory's root (e.g. for sdl it is Libraries/SDL/sdl32.dll)
