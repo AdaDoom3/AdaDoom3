4fe70e09051cda7ebb995e0d5751b1c6dbd7d49a
jsquirek1 <jsquirek1@student.gsu.edu>
2013-05-05 16:01:57 -0400

    Layout

--- Libraries/build.bat
+++
@@ -1,3 +0,0 @@
-.... TODO
-* overwrite all dlls with new versions if the compiler exists to create for the desired bit size library executable
-* the dll should all be in the source directory's root (e.g. for sdl Libraries/SDL/sdl32.dll)
