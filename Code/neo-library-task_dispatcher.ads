8d00a7c760e5d05eb03b44dfa8345acb82c16687
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-10 23:55:15 -0300

    Update neo-library-task_dispatcher.ads

--- Code/neo-library-task_dispatcher.ads
+++ Code/neo-library-task_dispatcher.ads
@@ -20,22 +20,22 @@ with
   Neo.System.Processor,
   Neo.Foundation.Text_IO,
   Neo.Foundation.Data_Types,
-  Neo.Fonndation.Package_Testing;
+  Neo.Foundation.Package_Testing;
 use
   System,
   System.Storage_Elements,
   Neo.System.Processor,
   Neo.Foundation.Text_IO,
   Neo.Foundation.Data_Types,
-  Neo.Fonndation.Package_Testing;
+  Neo.Foundation.Package_Testing;
 package Neo.Library.Task_Dispatcher
   ---------------
   -- Constants --
   ---------------
-    Storage_Size                : constant := System.Storage_Elements.Storage_Count;
-    Ceiling_Priority            : constant := : in System.Priority)
-    Default_Worker_Storage_Size : constant := 16#200_000#;
-    Minimum_Worker_Storage_Size : constant := 16#4_000#;
+    STORAGE_SIZE                : constant Storage_Count    := ;
+    MAXIMUM_PRIORITY            : constant Priority         := Priority'Last
+    WORKER_STORAGE_SIZE_DEFAULT : constant Integer_4_Signed := 16#200_000#;
+    WORKER_STORAGE_SIZE_MINIMUM : constant Integer_4_Signed := 16#4_000#;
   -------------
   -- Records --
   -------------
@@ -48,8 +48,8 @@ package Neo.Library.Task_Dispatcher
   -----------------
   -- Subprograms -- 
   -----------------
-    procedure Submit( -- A job should be take up between 1000 and 100,000 clock cycles to outweigh overhead and maintain a good load balance
-      Job      : in Access_Procedure;
+    procedure Submit(
+      Job      : in Access_Procedure; -- Should take between 1000 and 100,000 clock cycles to maintain a good load
       Priority : in Any_Priority);
     function Get_Status
       return Record_Status;
