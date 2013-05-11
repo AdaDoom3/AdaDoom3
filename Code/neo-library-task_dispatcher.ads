dae18970c0fac45d39fb517561e4f1594d1bac6b
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-11 04:14:09 -0300

    Update neo-library-task_dispatcher.ads

--- Code/neo-library-task_dispatcher.ads
+++ Code/neo-library-task_dispatcher.ads
@@ -35,6 +35,7 @@ package Neo.Library.Task_Dispatcher
     RECOMMENDED_JOB_EXECUTION_TICK_MIMIMUM    : constant := 1_000;
     RECOMMENDED_JOB_EXECUTION_TICK_MAXIMUM    : constant := 100_000;
     DO_PUT_WARNING_IF_JOB_VIOLATES_TICK_RANGE : constant Boolean := True;
+    
     STORAGE_SIZE                              : constant Storage_Count    := ;
     PRIORITY_MAXIMUM                          : constant Priority         := Priority'Last;
     WORKER_STORAGE_SIZE_DEFAULT               : constant Integer_4_Signed := 16#200_000#;
@@ -48,8 +49,6 @@ package Neo.Library.Task_Dispatcher
         Number_Of_Avaliable_Workers        : Worker_Count_Type     := ;
         Number_Of_Executed_Jobs            : := ;
         Last_Submission_Time_Started       : Time := ;
-        Last_Submission_Duration_Wasted    : Duration := 0.0;
-        Last_Submission_Duration           : Duration := 0.0;
         Average_Submission_Duration_Wasted : Duration := 0.0;
         Average_Submission_Duration        : Duration := 0.0;
         Worker_Stack_Size                  : Storage_Count := ;
@@ -71,25 +70,17 @@ private
   ---------------
     protected type Protected_Data
       is
-        function Get_Number_Of_Workers  
-        function Get_Number_Of_Avaliable_Workers 
-        function Get_Number_Of_Executed_Jobs   
-        function Get_Last_Submission_Time_Started 
-        function Get_Last_Submission_Duration_Wasted
-        function Get_Last_Submission_Duration   
-        function Get_Average_Submission_Duration_Wasted 
-        function Get_Average_Submission_Duration 
-        function Get_Worker_Stack_Size 
+        function Get_Status
+          return Record_Status;
+        procedure Set_Number_Of_Workers
+        procedure Set_Number_Of_Avaliable_Workers 
+        procedure Set_Number_Of_Executed_Jobs 
+        procedure Set_Last_Submission_Time_Started 
+        procedure Set_Worker_Stack_Size
+        procedure Add_To_Average_Submission_Duration_Wasted 
+        procedure Add_To_Average_Submission_Duration 
       private
-        Number_Of_Workers                  : Positive_Worker_Count := ;
-        Number_Of_Avaliable_Workers        : Worker_Count_Type     := ;
-        Number_Of_Executed_Jobs            : := ;
-        Last_Submission_Time_Started       : Time := ;
-        Last_Submission_Duration_Wasted    : Duration := 0.0;
-        Last_Submission_Duration           : Duration := 0.0;
-        Average_Submission_Duration_Wasted : Duration := 0.0;
-        Average_Submission_Duration        : Duration := 0.0;
-        Worker_Stack_Size                  : Storage_Count := ;
+        Status : Record_Status;
       end Protected_Data;
   --...
   end Neo.Library.Task_Dispatcher;
