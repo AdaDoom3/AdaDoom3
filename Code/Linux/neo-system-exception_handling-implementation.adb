16020e49c8d74036ee54050664e5a213be9247ba
AdaDoom3 <jsquirek1@student.gsu.edu>
2013-05-27 23:01:41 -0300

    Update linux stub

--- Code/Linux/neo-system-exception_handling-implementation.adb
+++ Code/Linux/neo-system-exception_handling-implementation.adb
@@ -20,14 +20,37 @@ use
   Neo.Linux;
 separate(Neo.System.Exception_Handling)
 package body Implementation
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+with
+  Neo.Linux;
+use
+  Neo.Linux;
+separate(Neo.System.Exception_Handling)
+package body Implementation
   is
-  -----------------
-  -- Start_Alert --
-  -----------------
+  ---------------
+  -- Set_Alert --
+  ---------------
     procedure Start_Alert
       is
       begin
-        raise System_Call_Failure;
+        raise Unsupported_Feature;
+        null;
       end Start_Alert;
   ----------------
   -- Stop_Alert --
@@ -35,18 +58,22 @@ package body Implementation
     procedure Stop_Alert
       is
       begin
-        raise System_Call_Failure;
+        raise Unsupported_Feature;
+        null;
       end Stop_Alert;
-  --------------------------
-  -- Create_Error_Console --
-  --------------------------
-    procedure Create_Error_Console(
-      Text    : in String_2;
-      Buttons : in Array_Console_Buttons)
+  -------------------
+  -- Spawn_Console --
+  -------------------
+    procedure Spawn_Console(
+      Title     : in String_2;
+      Text      : in String_2;
+      Buttons   : in Array_Record_Console_Button;
+      Icon_Path : in String_2 := NULL_STRING_2)
       is
       begin
-        raise System_Call_Failure;
-      end Create_Error_Console;
+        raise Unsupported_Feature;
+        null;
+      end Spawn_Console;
   -------------
   -- Is_Okay --
   -------------
@@ -54,12 +81,11 @@ package body Implementation
       Title        : in String_2;
       Message      : in String_2;
       Buttons      : in Enumerated_Buttons;
-      Icon         : in Enumerated_Icon;
-      Parent_Title : in String_2 := NULL_STRING_2)
+      Icon         : in Enumerated_Icon)
       return Boolean
       is
       begin
-        raise System_Call_Failure;
+        raise Unsupported_Feature;
         return False;
       end Is_Okay;
   end Implementation;
