29c714c0482f477c28718e47403f143565193af9
Artium <artium@nihamkin.com>
2015-11-28 18:37:56 +0200

    Reraise Unimplemented_Feature exception in main. Added a .gitignore file.

--- Engine/main.adb
+++ Engine/main.adb
@@ -11,14 +11,29 @@ with Neo.System.Text.Console;    use Neo.System.Text.Console;
 with Neo.System.Graphics.Window; use Neo.System.Graphics.Window;
 --with Neo.System.Sound;           use Neo.System.Sound;
 --with Neo.System.Network;         use Neo.System.Network;
+
 procedure Main is
-  begin
-    Set_Do_Put_Debug(True);
-    if Do_Put_Debug then Neo.System.Text.Console.Initialize; end if;
-    begin Run; exception when Occurrence: others => Handle_Exception(Occurrence); end;
-    if not Neo.System.Text.Console.Is_Running and then Do_Fail and then Is_Okay(
-      Icon    => Error_Icon,
+begin
+   Set_Do_Put_Debug(True);
+   
+   if Do_Put_Debug then 
+      Neo.System.Text.Console.Initialize; 
+   end if;
+   
+   begin Run; 
+   exception 
+      when Unimplemented_Feature => raise;
+      when Occurrence: others => Handle_Exception(Occurrence); 
+   end;
+   
+   if not Neo.System.Text.Console.Is_Running and then Do_Fail and then 
+     Is_Okay
+     (Icon    => Error_Icon,
       Name    => To_String_2(Neo.System.SPECIFICS.Name),
       Message => Localize("An error has occurred, would you like to view more information?"),
-      Buttons => Yes_No_Buttons) then Neo.System.Text.Console.Initialize; end if;
-  end Main;
+      Buttons => Yes_No_Buttons) then
+      
+      Neo.System.Text.Console.Initialize; 
+      
+   end if;
+end Main;
